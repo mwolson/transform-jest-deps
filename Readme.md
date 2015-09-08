17f52daeb19a98cd10b6cfca1ba2bf7952347b84
Michael Olson <mwolson@member.fsf.org>
2015-09-07 17:04:11 -0700

    Use falafel instead of acorn directly, allow custom acorn parser and options to be passed to it
    - The 'walk' library was inflexible about using custom parsers, since it hardcoded the types of nodes that it expects
      to see, which did not play well with acorn-jsx.  Using falafel was a workable alternative; it also has easier
      semantics for updating nodes, so it's no longer necessary to track file offset
    
    - Pass options to falafel, bringing in lodash.assign to make it easier to have default options
    
    - For backwards compatiblity, accept either a transform function or an options object as 2nd argument.  If an object,
      it must have a `requireTransform' key whose value is the transform function.
    
    - Write tests to verify that ES6 features are handled when passing in the ecmaVersion option.  No attempt is made to
      handle ES6 import statements yet.

--- Readme.md
+++ Readme.md
@@ -17,7 +17,33 @@ outputs:
 require('z'); require('y')
 ```
 
+[options can be passed to falafel](https://github.com/substack/node-falafel#custom-parser) by using an object as the
+second argument.  if this is done, pass the transform function as the `requireTransform` key to the options object.
+
+```js
+var acorn = require('acorn-jsx');
+var transform = require('transform-deps');
+var src = [
+    "require('x');",
+    "function Thing() {}",
+    "var foo = <Thing/>;",
+    "var arr1 = [1]; var arr2 = [...arr1, 2]",
+    "require('y');"
+].join("\n");
+src = transform(src, {
+    ecmaVersion: 6,
+    parser: acorn,
+    plugins: {jsx: true},
+    requireTransform: function(name) {
+        if (name == 'x') return 'z';
+    }
+});
+console.log(src);
+```
+
 # api
+
+## var s = transform(src, options, ignore_trycatch=false)
 
 ## var s = transform(src, cb, ignore_trycatch=false)
 
--- index.js
+++ index.js
@@ -1,31 +1,52 @@
-var acorn = require('acorn');
-var walk = require('acorn/util/walk');
+var assign = require('lodash.assign');
+var falafel = require('falafel');
 
-module.exports = function (src, fn, ignore_trycatch) {
-  var ret = src;
-  var ast = acorn.parse(src, { ranges: true });
-  var offset = 0;
-  walk.ancestor(ast, {
-    CallExpression: function (node, state) {
-      if (node.callee.type === 'Identifier' && 
-          node.callee.name === 'require' && node.arguments) {
-        if (ignore_trycatch) {
-          var istrycatch = state.some(function (s) {
-            return s.type === 'TryStatement' || s.type === 'CatchClause';
-          });
-          if (istrycatch)
-            return;
-        }
-        var arg0 = node.arguments[0];
-        var value = src.substring(arg0.range[0] + 1, arg0.range[1] - 1);
-        var update = fn(value);
-        if (!update || typeof update !== 'string')
+function parents(node) {
+  var parents = [];
+  for (var p = node.parent; p; p = p.parent) {
+    parents.push(p);
+  }
+  return parents;
+}
+
+var defaultOptions = {
+  ranges: true
+};
+
+module.exports = function (src, options, ignore_trycatch) {
+  var fn;
+  if (typeof options === 'function') {
+    fn = options;
+    options = defaultOptions;
+  } else {
+    fn = options.requireTransform;
+    options.requireTransform = undefined;
+    options = assign({}, defaultOptions, options);
+  }
+
+  return falafel(src, options, function(node) {
+    if (node.type === 'CallExpression' &&
+        node.callee.type === 'Identifier' &&
+        node.callee.name === 'require' && node.arguments) {
+      if (ignore_trycatch) {
+        var istrycatch = parents(node).some(function (s) {
+          return s.type === 'TryStatement' || s.type === 'CatchClause';
+        });
+        if (istrycatch)
           return;
-        ret = ret.substring(0, arg0.range[0] + 1 + offset) + 
-          update + ret.substring(arg0.range[1] - 1 + offset);
-        offset += update.length - value.length;
       }
+      var arg0 = node.arguments[0];
+      var value = src.substring(arg0.start + 1, arg0.end - 1);
+      var update = fn(value);
+      if (!update || typeof update !== 'string')
+        return;
+      var nodesrc = node.source();
+      var parts = [
+        nodesrc.substring(0, arg0.start - node.start + 1),
+        update,
+        nodesrc.substring(arg0.end - node.start - 1)
+      ];
+      node.update(parts.join(''));
     }
-  });
-  return ret;
+  }).toString();
 };
