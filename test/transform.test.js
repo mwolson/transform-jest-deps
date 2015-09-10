66c330a6307c68950a0c6765b25fd735cb939582
Michael Olson <mwolson@member.fsf.org>
2015-09-09 17:39:21 -0700

    Bundle acorn-jsx and use ES6 by default

--- test/transform.test.js
+++ test/transform.test.js
@@ -4,18 +4,14 @@ var rewire = require('rewire');
 var sinon = require('sinon');
 
 describe('transform-jest-deps module', function() {
-  var expected, falafel, mappings, src, transform;
+  var defaultOptions, expected, falafel, mappings, src, transform;
 
   function replaceDep(dep) {
     return mappings[dep];
   }
 
   function verifyFalafel(options) {
-    options = options ? _.clone(options) : {};
-    _.assign(options, { ranges: true });
-    if (options.hasOwnProperty('ignoreTryCatch')) {
-      delete options.ignoreTryCatch;
-    }
+    options = _.omit(_.assign({}, defaultOptions, options), 'ignoreTryCatch');
 
     expect(falafel).to.be.calledOnce;
     expect(falafel.args[0][0]).to.eq(src);
@@ -25,6 +21,7 @@ describe('transform-jest-deps module', function() {
 
   beforeEach(function() {
     transform = rewire('../lib/transform');
+    defaultOptions = transform.__get__('defaultOptions');
     falafel = sinon.spy(transform.__get__('falafel'));
     transform.__set__('falafel', falafel);
 
@@ -59,9 +56,8 @@ describe('transform-jest-deps module', function() {
     });
 
     it('works with newer API', function() {
-      var options = {};
-      var res = transform(src, options, replaceDep);
-      verifyFalafel(options);
+      var res = transform(src, replaceDep);
+      verifyFalafel();
       expect(res).to.eq(expected);
     });
   });
@@ -88,16 +84,15 @@ describe('transform-jest-deps module', function() {
       src = "try { require('a') } catch (e) { require('b'); } require('c');";
     });
 
-    it('with ignore_trycatch option disabled, replaces all', function() {
+    it('with ignoreTryCatch=false, replaces all', function() {
       expected = "try { require('x') } catch (e) { require('x'); } require('x');";
-      var options = {};
 
-      var res = transform(src, options, function() { return 'x'; });
-      verifyFalafel(options);
+      var res = transform(src, function() { return 'x'; });
+      verifyFalafel();
       expect(res).to.eq(expected);
     });
 
-    it('with ignore_trycatch option enabled, replaces outside of the try/catch', function() {
+    it('with ignoreTryCatch=true, replaces outside of the try/catch', function() {
       expected = "try { require('a') } catch (e) { require('b'); } require('x');";
       var options = { ignoreTryCatch: true };
 
@@ -119,10 +114,28 @@ describe('transform-jest-deps module', function() {
         "var arr2 = [...arr1, 'val2'];",
         "var fs = require('fsx');"
       ].join("\n");
-      var options = { ecmaVersion: 6 };
 
-      var res = transform(src, options,  replaceDep);
-      verifyFalafel(options);
+      var res = transform(src, replaceDep);
+      verifyFalafel();
+      expect(res).to.eq(expected);
+    });
+  });
+
+  describe('with JSX features', function() {
+    it('replaces deps', function() {
+      src = [
+        "function Thing() {}",
+        "var thing = (<Thing/>);",
+        "var fs = require('fs');"
+      ].join("\n");
+      expected = [
+        "function Thing() {}",
+        "var thing = (<Thing/>);",
+        "var fs = require('fsx');"
+      ].join("\n");
+
+      var res = transform(src,  replaceDep);
+      verifyFalafel();
       expect(res).to.eq(expected);
     });
   });
@@ -131,50 +144,45 @@ describe('transform-jest-deps module', function() {
     it('replaces in simple statement', function() {
       src = "jest.dontMock('fs');";
       expected = "jest.dontMock('fsx');";
-      var options = {};
 
-      var res = transform(src, options,  replaceDep);
-      verifyFalafel(options);
+      var res = transform(src,  replaceDep);
+      verifyFalafel();
       expect(res).to.eq(expected);
     });
 
     it('replaces in compound statement', function() {
       src = "jest.dontMock('fs').dontMock('path').mock(\"util\");";
       expected = "jest.dontMock('fsx').dontMock('./path').mock(\"zxcqlw\");";
-      var options = {};
 
-      var res = transform(src, options,  replaceDep);
-      verifyFalafel(options);
+      var res = transform(src,  replaceDep);
+      verifyFalafel();
       expect(res).to.eq(expected);
     });
 
     it('replaces in genMockFromModule', function() {
       src = "jest.dontMock('fs').genMockFromModule('path');";
       expected = "jest.dontMock('fsx').genMockFromModule('./path');";
-      var options = {};
 
-      var res = transform(src, options,  replaceDep);
-      verifyFalafel(options);
+      var res = transform(src,  replaceDep);
+      verifyFalafel();
       expect(res).to.eq(expected);
     });
 
     it('replaces in setMock', function() {
       src = "jest.dontMock('fs').setMock('path', {});";
       expected = "jest.dontMock('fsx').setMock('./path', {});";
-      var options = {};
 
-      var res = transform(src, options,  replaceDep);
-      verifyFalafel(options);
+      var res = transform(src,  replaceDep);
+      verifyFalafel();
       expect(res).to.eq(expected);
     });
 
     it('replaces in require.requireActual', function() {
       src = "require.requireActual('fs');";
       expected = "require.requireActual('fsx');";
-      var options = {};
 
-      var res = transform(src, options,  replaceDep);
-      verifyFalafel(options);
+      var res = transform(src,  replaceDep);
+      verifyFalafel();
       expect(res).to.eq(expected);
     });
   });
